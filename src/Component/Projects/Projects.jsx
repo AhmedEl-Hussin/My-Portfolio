801e7769001439fe0819baafd8c64c10f5d2c34a
Ahmed Elhussin <ahmedelhussin@192.168.1.8>
2024-02-16 16:48:35 +0200

    fixeProject

--- src/Component/Projects/Projects.jsx
+++ src/Component/Projects/Projects.jsx
@@ -1,8 +1,9 @@
 
 export default function Projects() {
     return (
-        <div>
-            <h1>Ahmed Moahmed</h1>
-        </div>
+        <>
+            <h1 className="test">Ahmed Moahmed</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque ducimus aperiam inventore, tempore minus nisi rerum molestiae culpa illo natus consectetur necessitatibus. Quo possimus eos iure sapiente, tenetur ut aliquid?</p>
+        </>
     )
 }
